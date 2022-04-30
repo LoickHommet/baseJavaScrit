ac49d4e1b539b8698f902d6e51c831e5e6a1a076
Loick Hommet <hommet.loick@gmail.com>
2022-04-30 14:40:41 +0200

    Deéclarer les variables et modifier leur valeur

---
+++ variable.js
@@ -0,0 +1,24 @@
+let costPerProduct = 10
+let numberOfProducts = 5
+let totalCost = costPerProduct * numberOfProducts
+let avergaCostPerProduct = costPerProduct / numberOfProducts
+
+
+
+let episodeTime = 45
+let commercialTime = 5
+
+let numberEpisode = 12
+let numberSeris = 2
+
+let totalEpisode = numberEpisode * numberEpisode
+
+let tolalTimeEpisode = episodeTime + commercialTime
+
+let totalShowTime = tolalTimeEpisode * commercialTime
+
+console.log(totalShowTime)
+
+const hoursPerDay = 24
+const minituesPerHour =  60
+const secondsPerMinute = 60
