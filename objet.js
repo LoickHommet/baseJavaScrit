d23f0a337b2d96441b6213e6793450fb24d4997b
Loick Hommet <hommet.loick@gmail.com>
2022-04-30 15:16:33 +0200

    utilisation de class

--- objet.js
+++ objet.js
@@ -1,19 +1,25 @@
 
 
-let episode = {
-    title : "Je suis encore dessus",
-    duration : 50,
-    hasBeenWatched : false
-
+class Episode {
+     constructor(title, duration, hasBeenWatched){
+         this.title = title,
+         this.duration =  duration,
+         this.hasBeenWatched = hasBeenWatched
+     }
 }
 
-let episodeTilte = episode['title']
-let episodeDuration = episode['duration']
-let episodeHasBeenWatched = episode['hasBeenWatched']
+let fristEpisode = new Episode("Il est 15:11", 54, false)
+let sencondEpisode = new Episode("Il est toujour 15:11", 20 , true)
 
 
 document.querySelector("#episode-info").innerHTML = 
-`Titre : ${episodeTilte} <br>
-Duree : ${episodeDuration} <br>
-${episodeHasBeenWatched ? 'DEjas vu ' : "pas vue"}
+`Titre : ${fristEpisode.title} <br>
+Duree : ${fristEpisode.duration} <br>
+${fristEpisode.hasBeenWatched ? 'DEjas vu ' : "pas vue"}
+`
+
+document.querySelector("#episode-info2").innerHTML = 
+`Titre ${sencondEpisode.title} <br>
+Duree ${sencondEpisode.duration} <br>
+ ${sencondEpisode.hasBeenWatched ? 'dejas vu ' : "pas vue"} <br>
 `
